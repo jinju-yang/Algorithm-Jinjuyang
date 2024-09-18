24f68ca9eee48ce9952336cd252f7431d120e0c7
jinju-yang <129378360+jinju-yang@users.noreply.github.com>
2024-09-19 02:24:29 +0900

    [Bronze V] Title: 오늘 날짜, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10699. 오늘 날짜/오늘 날짜.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <algorithm>
+#include <cstring>
+#include <string>
+#include <cmath>
+#include <vector>
+#define fio ios_base::sync_with_stdio(0), cin.tie(0)
+using namespace std;
+
+int main(void)
+{		
+	cout << "2024-05-28";
+	return 0;
+}
