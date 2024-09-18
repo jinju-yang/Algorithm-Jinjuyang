8721306afed1b978e92d0edeafff0d2fcc364311
jinju-yang <129378360+jinju-yang@users.noreply.github.com>
2024-09-19 02:22:32 +0900

    [Silver IV] Title: 괄호, Time: 4 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/9012. 괄호/괄호.cc
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <stack>
+#include <string>
+
+using namespace std;
+
+int main() 
+{
+    int n;
+    cin >> n;
+   
+    while(n--) 
+    {
+        stack<char> st;
+        string s;
+        cin >> s;
+        
+        for(int j = 0; j < s.length(); j++) 
+        {
+            if(st.empty()) 
+            { 
+                st.push(s[j]);
+            }
+            else 
+            { 
+                if(st.top() == '(' && s[j] == ')') 
+                {
+                    st.pop();//Pop
+                }
+                else 
+                {
+                    st.push(s[j]);
+                }
+            }
+        }
+        if(st.empty()) 
+        {
+            cout << "YES" << "\n";
+        }
+        else 
+        {
+            cout << "NO" << "\n";
+        }
+    }
+}
