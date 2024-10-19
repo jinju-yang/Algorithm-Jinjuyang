f7040281b29b195c8d93c03081bc88371a3d3656
jinju-yang <129378360+jinju-yang@users.noreply.github.com>
2024-10-20 02:00:18 +0900

    [Silver V] Title: 수 정렬하기 4, Time: 312 ms, Memory: 80144 KB -BaekjoonHub

---
+++ 백준/Silver/11931. 수 정렬하기 4/수 정렬하기 4.cc
@@ -0,0 +1,43 @@
+#include <iostream>
+using namespace std;
+
+int N;
+int arr[10000000];
+int tmp[10000000];
+
+void merge(int st, int en) {
+  int mid = (st + en) / 2;
+  int lidx = st;
+  int ridx = mid;
+  for(int i = st; i < en; i++) {
+    if(lidx == mid) tmp[i] = arr[ridx++];
+    else if(ridx == en) tmp[i] = arr[lidx++];
+    else if(arr[lidx] >= arr[ridx]) tmp[i] = arr[lidx++];
+    else tmp[i] = arr[ridx++];
+  }
+  for(int i = st; i < en; i++) arr[i] = tmp[i];
+}
+
+void merge_sort(int st, int en) {
+  if(en == st + 1) return;
+  int mid = (st + en) / 2;
+  merge_sort(st, mid);
+  merge_sort(mid, en);
+  merge(st, en);
+}
+
+int main(void){
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+  cout.tie(0);
+
+  cin >> N;
+  for(int i = 0; i < N; i++) {
+    cin >> arr[i];
+  }
+
+  merge_sort(0, N);
+
+  for(int i = 0; i < N; i++) cout << arr[i] << "\n";
+
+}
