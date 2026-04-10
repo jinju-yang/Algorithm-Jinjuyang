4c983bd0c0f1feec6865139446d02c6a876e48e5
jinju-yang <129378360+jinju-yang@users.noreply.github.com>
2026-04-10 23:12:51 +0900

    [Bronze II] Title: 숫자, Time: 12 ms, Memory: 2020 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/10093. 숫자/숫자.cc
+++ 백준/Bronze/10093. 숫자/숫자.cc
@@ -1,29 +1,20 @@
 #include <iostream>
-#include <algorithm>
-#include <cstring>
-#include <string>
-#include <cmath>
-#include <vector>
-#define fio ios_base::sync_with_stdio(0), cin.tie(0)
+
 using namespace std;
 
-int main(void)
-{
-	long num[2];
-	for (int i = 0; i < 2; i++) {
-		cin >> num[i];
-	}
-	sort(num, num + 2);
+long long max(long long A, long long B) { return (A >= B) ? A : B; }
 
-	if (num[0] == num[1]) {
-		cout << 0;
-	}
-	else {
-		cout << num[1] - num[0] - 1 << endl;
-		for (int i = 1; i < num[1] - num[0]; i++) {
-			cout << num[0] + i << " ";
-		}
-	}
-	
-	return 0;
+long long min(long long A, long long B) { return (A <= B) ? A : B; }
+
+int main() {
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+
+  long long A, B;
+  cin >> A >> B;
+  (A == B) ? cout << 0 : cout << max(A, B) - min(A, B) - 1 << "\n";
+  for (long long i = min(A, B) + 1; i < max(A, B); i++) cout << i << " ";
+
+  return 0;
 }
+
