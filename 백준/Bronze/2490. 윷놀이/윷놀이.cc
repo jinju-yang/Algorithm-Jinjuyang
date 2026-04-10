16f0b670d3b4697c0cf50f86bb0ea0bd569402aa
jinju-yang <129378360+jinju-yang@users.noreply.github.com>
2026-04-10 21:54:07 +0900

    [Bronze III] Title: 윷놀이, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/2490. 윷놀이/윷놀이.cc
+++ 백준/Bronze/2490. 윷놀이/윷놀이.cc
@@ -1,38 +1,17 @@
 #include <iostream>
-#include <algorithm>
-#include <cstring>
-#include <string>
-#include <cmath>
-#include <vector>
-#define fio ios_base::sync_with_stdio(0), cin.tie(0)
 using namespace std;
 
-int main(void)
-{
-	for (int i = 0; i < 3; i++) {
-		int yoot;
-		int result = 0;
-		for (int j = 0; j < 4; j++) {
-			cin >> yoot;
-			result += yoot;
-		}
-		switch (result) {
-		case 0:
-			cout << "D" << endl;
-			break;
-		case 1:
-			cout << "C" << endl;
-			break;
-		case 2:
-			cout << "B" << endl;
-			break;
-		case 3: 
-			cout << "A" << endl;
-			break;
-		case 4:
-			cout << "E" << endl;
-			break;
-		}
-	}
-	return 0;
+int main() {
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+
+  int A, B, C, D;
+
+  for (int i = 0; i < 3; i++) {
+    cin >> A >> B >> C >> D;
+    int sum = A + B + C + D;
+    cout << "DCBAE"[sum] << "\n";
+  }
+
+  return 0;
 }
