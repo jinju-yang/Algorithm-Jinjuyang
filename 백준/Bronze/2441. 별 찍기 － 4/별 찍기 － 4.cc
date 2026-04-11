f8f68113ae9651162a7f625c047a026ec0289c9d
jinju-yang <129378360+jinju-yang@users.noreply.github.com>
2026-04-11 22:03:14 +0900

    [Bronze III] Title: 별 찍기 - 4, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/2441. 별 찍기 － 4/별 찍기 － 4.cc
+++ 백준/Bronze/2441. 별 찍기 － 4/별 찍기 － 4.cc
@@ -1,25 +1,16 @@
 #include <iostream>
-#include <algorithm>
-#include <cstring>
-#include <string>
-#include <cmath>
-#include <vector>
-#define fio ios_base::sync_with_stdio(0), cin.tie(0)
 using namespace std;
 
-int N;
+int main() {
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+  int N;
+  cin >> N;
 
-int main(void)
-{
-	cin >> N;
-	for (int i = N; i > 0; i--) {
-		for (int j = 0; j < (N - i); j++) {
-			cout << " ";
-		}
-		for (int j = i; j > 0; j--) {
-			cout << "*";
-		}
-		cout << "\n";
-	}
-	return 0;
+  for (int i = N; i > 0; i--) {
+    for (int k = 0; k < N - i; k++) cout << " ";
+    for (int j = i; j > 0; j--) cout << "*";
+    cout << "\n";
+  }
+  return 0;
 }
