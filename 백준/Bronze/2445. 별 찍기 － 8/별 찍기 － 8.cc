8dae5c0aaa104744920ba9b5f757f2f1e487c484
jinju-yang <129378360+jinju-yang@users.noreply.github.com>
2026-04-13 21:29:56 +0900

    [Bronze III] Title: 별 찍기 - 8, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/2445. 별 찍기 － 8/별 찍기 － 8.cc
+++ 백준/Bronze/2445. 별 찍기 － 8/별 찍기 － 8.cc
@@ -1,41 +1,25 @@
 #include <iostream>
-#include <algorithm>
-#include <cstring>
-#include <string>
-#include <cmath>
-#include <vector>
-#define fio ios_base::sync_with_stdio(0), cin.tie(0)
 using namespace std;
 
-int main(void)
-{
-	int N;
-	cin >> N;
+int main() {
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+  int N;
+  cin >> N;
 
-	for (int i = 1; i < (N * 2); i++) {
-		if (i <= N) {
-			for (int j = 1; j <= i; j++) {
-				cout << "*";
-			}
-			for (int j = (N - i) * 2; j >= 1; j--) {
-				cout << " ";
-			}
-			for (int j = 1; j <= i; j++) {
-				cout << "*";
-			}
-		}
-		else {
-			for (int j = (i - N); j < N; j++) {
-				cout << "*";
-			}
-			for (int j = (i - N) * 2; j >= 1; j--) {
-				cout << " ";
-			}
-			for (int j = (i - N); j < N; j++) {
-				cout << "*";
-			}
-		}
-		cout << "\n";
-	}
-	return 0;
+  for (int i = 0; i < 2 * N; i++) {
+    int j = 0;
+    if (i < N) {
+      for (; j < i + 1; j++) cout << "*";
+      for (; j < 2 * N - i - 1; j++) cout << " ";
+      for (; j < 2 * N; j++) cout << "*";
+    } else {
+      for (; j < 2 * N - i - 1; j++) cout << "*";
+      for (; j < i + 1; j++) cout << " ";
+      for (; j < 2 * N; j++) cout << "*";
+    }
+    cout << "\n";
+  }
+
+  return 0;
 }
